24fb0b19ce286d48eb8fa185c30a47f6208fc3e8
Tony Maluki <tonymaluki@gmail.com>
2023-02-01 14:04:17 +0300

    small changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 const App = () => {
   return (
     <>
       <Navbar />
+      <Home />
       <Footer />
     </>
   );
