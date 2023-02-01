67b7fa75cdeba1d993d331de873437f2e5c42840
Tony Maluki <tonymaluki@gmail.com>
2023-02-02 00:04:17 +0300

    created a social tab component

---
+++ src/components/Socials.jsx
@@ -0,0 +1,35 @@
+const Socials = () => {
+  return (
+    <div className="contacts-icons">
+      <a href="https://github.com/G-69westside" target="_blank">
+        <i className="fa-brands fa-github"></i>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/tony-maluki-9b9611173/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i className="fa-brands fa-linkedin-in"></i>
+      </a>
+      <a
+        href="https://www.instagram.com/life.astony/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i className="fa-brands fa-instagram"></i>
+      </a>
+      <a
+        href="https://twitter.com/asap_toni"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i className="fa-brands fa-twitter"></i>
+      </a>
+      <a href="mailto:tonymaluki@gmail.com">
+        <i class="fa-solid fa-envelope"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Socials;
