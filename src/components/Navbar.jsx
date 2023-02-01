ed62ffadfd5c9d760ee5c41cc2e0ed3cc76dc1fc
Tony Maluki <tonymaluki@gmail.com>
2023-02-01 12:29:41 +0300

    working on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,25 @@
 const Navbar = () => {
-    return ( 
-        <div className="navbar-container">
-            <p>i want to test this out</p>
+  return (
+    <section className="navbar">
+      <div className="nav-container">
+        <div className="nav-item">
+          <a href="" className="Nav-link">
+            Home
+          </a>
         </div>
-     );
-}
- 
+        <div className="nav-item">
+          <a href="" className="Nav-link">
+            About
+          </a>
+        </div>
+        <div className="nav-item">
+          <a href="" className="Nav-link">
+            Projects
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
 export default Navbar;
