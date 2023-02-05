a138bbf20fba1214ca764e7f2acfae00823694be
Tony Maluki <tonymaluki@gmail.com>
2023-02-06 00:40:52 +0300

    testdata

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,3 +1,36 @@
+import { nanoid } from "nanoid";
+import icta from "./assets/pictures/ictalogo.jpg";
+// export default [
+//   {
+//     name: "Tony Ndereva Maluki",},
+//     projects: {
+//       id: nanoid(),
+//       projectName: "ICT Authority",
+//       projectImage: icta,
+//       projectDescription:
+//         "A sub branch of an ERP system from KwikBasket Solutions Nairobi Kenya that helps internal teams improve their work output. Most of he systems that have been developed here are utility apps.Kwikbasket solutions is an agrucltural firm that consists of a team of passionate agriculture enthusiasts and techies committed to changing farmers lives by improving their access to fair markets and providing high quality fresh produce to our customers",
+//     }
+//   },
+// ];
 export default [
-    
-]
+  
+  {
+    name: 'Tony Ndereva maluki',
+    projects: {
+      id: nanoid(),
+      projectName: "ICT Authority",
+      projectImage: icta,
+      projectDescription:
+        "A sub branch of an ERP system from KwikBasket Solutions Nairobi Kenya that helps internal teams improve their work output. Most of he systems that have been developed here are utility apps.Kwikbasket solutions is an agrucltural firm that consists of a team of passionate agriculture enthusiasts and techies committed to changing farmers lives by improving their access to fair markets and providing high quality fresh produce to our customers",
+    },
+  },
+  {
+    projects: {
+      id: nanoid(),
+      projectName: "Connected KENYA",
+      projectImage: icta,
+      projectDescription:
+        "A sub branch of an ERP system from KwikBasket Solutions Nairobi Kenya that helps internal teams improve their work output. Most of he systems that have been developed here are utility apps.Kwikbasket solutions is an agrucltural firm that consists of a team of passionate agriculture enthusiasts and techies committed to changing farmers lives by improving their access to fair markets and providing high quality fresh produce to our customers",
+    },
+  },
+];
