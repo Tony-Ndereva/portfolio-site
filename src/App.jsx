c32c06267121e9f5dbed7c06766da19d97455c3e
Tony Maluki <tonymaluki@gmail.com>
2023-02-05 20:01:29 +0300

    Added route to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-
+import TestFunction from "./components/Test";
+import Projects from "./components/Projects";
+import { Routes, Route } from "react-router-dom";
 const App = () => {
   return (
     <>
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/Test" element={<TestFunction />}></Route>
+        <Route path="/Projects" element={<Projects />}></Route>
+      </Routes>
       <Footer />
     </>
   );
