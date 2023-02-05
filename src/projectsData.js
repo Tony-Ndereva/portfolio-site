d0f54b0fa0ade2487dc677165fe891445250991d
Tony Maluki <tonymaluki@gmail.com>
2023-02-06 00:40:05 +0300

    refactored data

---
+++ src/projectsData.js
@@ -0,0 +1,16 @@
+export default [
+  {
+    projects: {
+      projectName: "ICT Authority",
+      projectImage: icta,
+      projectDescription:
+        "A sub branch of an ERP system from KwikBasket Solutions Nairobi Kenya that helps internal teams improve their work output. Most of he systems that have been developed here are utility apps.Kwikbasket solutions is an agrucltural firm that consists of a team of passionate agriculture enthusiasts and techies committed to changing farmers lives by improving their access to fair markets and providing high quality fresh produce to our customers",
+    },
+    projects: {
+      projectName: "ICT Authority",
+      projectImage: icta,
+      projectDescription:
+        "A sub branch of an ERP system from KwikBasket Solutions Nairobi Kenya that helps internal teams improve their work output. Most of he systems that have been developed here are utility apps.Kwikbasket solutions is an agrucltural firm that consists of a team of passionate agriculture enthusiasts and techies committed to changing farmers lives by improving their access to fair markets and providing high quality fresh produce to our customers",
+    },
+  },
+];
