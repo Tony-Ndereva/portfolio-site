fdb15343a0179b3ec9730243e6a5094ba412635c
Tony Maluki <tonymaluki@gmail.com>
2023-02-06 00:38:59 +0300

    mapped projects

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,33 @@ import Footer from "./components/Footer";
 import TestFunction from "./components/Test";
 import Projects from "./components/Projects";
 import { Routes, Route } from "react-router-dom";
+import resumeData from "./resumeData";
 const App = () => {
+  const projects = resumeData.map((each) => {
+    return (
+      <Projects
+        projectImage={each.projects.projectImage}
+        projectName={each.projects.projectName}
+        projectDescription={each.projects.projectDescription}
+        key={each.projects.id}
+      />
+    );
+  });
+
   return (
     <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/Test" element={<TestFunction />}></Route>
-        <Route path="/Projects" element={<Projects />}></Route>
+        <Route
+          path="/Projects"
+          element={
+            <section className="project-section">
+              <div className="project-container">{projects}</div>
+            </section>
+          }
+        ></Route>
       </Routes>
       <Footer />
     </>
