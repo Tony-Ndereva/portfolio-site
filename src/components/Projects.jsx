db2eadfef888d86f72af158962e201fb68311d27
Tony Maluki <tonymaluki@gmail.com>
2023-02-06 12:29:46 +0300

    worked on project links

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,12 +1,15 @@
 const Projects = (props) => {
   return (
-    
+    <>
       <div className="project-card">
         <img src={props.projectImage} className="project-image" alt="" />
-        <h3>{props.projectName}</h3>
-        <p>{props.projectDescription}</p>
+        <h3 className="project-title">{props.projectName}</h3>
+        <p className="project-description">{props.projectDescription}</p>
+        <a className="project-link" href={props.projectLink} target="_blank" rel="noopener noreferrer">
+        <i className="fa-solid fa-link"></i> View Project
+        </a>
       </div>
-   
+    </>
   );
 };
 
