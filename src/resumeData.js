572042274ac68509521cc1d74e4a0b9ad76c2678
Tony Maluki <tonymaluki@gmail.com>
2023-02-06 12:30:30 +0300

    added test data

--- src/resumeData.js
+++ src/resumeData.js
@@ -13,12 +13,21 @@ import icta from "./assets/pictures/ictalogo.jpg";
 //   },
 // ];
 export default [
-  
   {
-    name: 'Tony Ndereva maluki',
+    name: "Tony Ndereva maluki",
     projects: {
       id: nanoid(),
       projectName: "ICT Authority",
+      projectImage: icta,
+      projectDescription:
+        "A sub branch of an ERP system from KwikBasket Solutions Nairobi Kenya that helps internal teams improve their work output. Most of he systems that have been developed here are utility apps.Kwikbasket solutions is an agrucltural firm that consists of a team of passionate agriculture enthusiasts and techies committed to changing farmers lives by improving their access to fair markets and providing high quality fresh produce to our customers",
+      projectLink: "https://icta.go.ke",
+    },
+  },
+  {
+    projects: {
+      id: nanoid(),
+      projectName: "Connected KENYA",
       projectImage: icta,
       projectDescription:
         "A sub branch of an ERP system from KwikBasket Solutions Nairobi Kenya that helps internal teams improve their work output. Most of he systems that have been developed here are utility apps.Kwikbasket solutions is an agrucltural firm that consists of a team of passionate agriculture enthusiasts and techies committed to changing farmers lives by improving their access to fair markets and providing high quality fresh produce to our customers",
@@ -33,4 +42,14 @@ export default [
         "A sub branch of an ERP system from KwikBasket Solutions Nairobi Kenya that helps internal teams improve their work output. Most of he systems that have been developed here are utility apps.Kwikbasket solutions is an agrucltural firm that consists of a team of passionate agriculture enthusiasts and techies committed to changing farmers lives by improving their access to fair markets and providing high quality fresh produce to our customers",
     },
   },
+  {
+    projects: {
+      id: nanoid(),
+      projectName: "Connected KENYA",
+      projectImage: icta,
+      projectDescription:
+        "A sub branch of an ERP system from KwikBasket Solutions Nairobi Kenya that helps internal teams improve their work output. Most of he systems that have been developed here are utility apps.Kwikbasket solutions is an agrucltural firm that consists of a team of passionate agriculture enthusiasts and techies committed to changing farmers lives by improving their access to fair markets and providing high quality fresh produce to our customers",
+    },
+  },
+  
 ];
