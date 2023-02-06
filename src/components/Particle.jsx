8257c39722318b00bbf094497651dc7d6c14605a
Tony Maluki <tonymaluki@gmail.com>
2023-02-07 02:41:43 +0300

    refactored particles

--- src/components/Particle.jsx
+++ src/components/Particle.jsx
@@ -3,38 +3,40 @@ import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
 
 const Particle = () => {
+  const init = useCallback(async (engine) => {
+    await loadFull(engine);
+  });
 
-    const init = useCallback(async (engine)=>{
-        await loadFull(engine)
-    })
+  return (
+    <Particles
+      options={{
+        particles: {
+          color: {
+            value: "#fff",
+          },
+          number: {
+            value: 100,
+          },
+          opacity: {
+            value: { min: 0.3, max: 0.4 },
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            value: { min: 1, max: 5 },
+          },
+          move: {
+            direction: "bottom-right",
+            enable: true,
+            speed: { min: 1, max: 2 },
+            straight: true,
+          },
+        },
+      }}
+      init={init}
+    />
+  );
+};
 
-    return (
-        <Particles options={{
-            particles:{
-                color:{
-                    value:"#fff"
-                },
-                number:{
-                    value:100
-                },
-                opacity:{
-                    value:{min:0.3,max:0.4}
-                },
-                shape:{
-                    type:"circle"
-                },
-                size:{
-                    value:{min:1,max:5}
-                },
-                move:{
-                    direction:'bottom-right',
-                    enable:true,
-                    speed:{min:1,max:2},
-                    straight:true
-                }
-            }
-        }} init={init}/>
-    )
-}
- 
 export default Particle;
