df8265fce1d55af07e99111f1b036994c43ebd5b
Tony Maluki <tonymaluki@gmail.com>
2023-02-16 23:33:49 +0300

    playing with context API

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,10 @@ import { Routes, Route } from "react-router-dom";
 import resumeData from "./resumeData";
 import Particle from "./components/Particle";
 import Tonydetails from "./Tonydetails";
-import { useState } from "react";
+import { useState, createContext } from "react";
+
+export const ThemeContext = createContext();
+
 const App = () => {
   const [projectsData, setProjectsData] = useState(resumeData);
   const [userData, setUserData] = useState(Tonydetails);
@@ -22,29 +25,31 @@ const App = () => {
   });
 
   return (
-    <div>
-      <Particle />
-      <Navbar />
-      <Routes>
-        <Route path="/" element={home}></Route>
-        <Route
-          path="/Projects"
-          element={
-            <section className="project-section">
-              <div className="project-intro">
-                <h1>
-                  My Recent <span className="orange">Works</span>
-                </h1>
-                <h3>Here are afew projects I've worked on recently.</h3>
-              </div>
-              <div className="project-container">{projects}</div>
-            </section>
-          }
-        ></Route>
-        <Route path="/Skills" element={<Skills />}></Route>
-      </Routes>
-      {footer}
-    </div>
+    <ThemeContext.Provider>
+      <div>
+        <Particle />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={home}></Route>
+          <Route
+            path="/Projects"
+            element={
+              <section className="project-section">
+                <div className="project-intro">
+                  <h1>
+                    My Recent <span className="orange">Works</span>
+                  </h1>
+                  <h3>Here are afew projects I've worked on recently.</h3>
+                </div>
+                <div className="project-container">{projects}</div>
+              </section>
+            }
+          ></Route>
+          <Route path="/Skills" element={<Skills />}></Route>
+        </Routes>
+        {footer}
+      </div>
+    </ThemeContext.Provider>
   );
 };
 
