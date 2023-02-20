b67f5b76f637cdf3769a9e35055a24afbe103a41
Tony Maluki <tonymaluki@gmail.com>
2023-02-20 15:24:47 +0300

    added trackers to projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,8 @@
+import GAEventsTracker from "./GAEventsTracker";
 const Projects = (props) => {
+  const EventsTracker = GAEventsTracker("External Project Links");
   return (
     <>
-    
       <div className="project-card">
         <img src={props.projectImage} className="project-image" alt="" />
         <h3 className="project-title">{props.projectName}</h3>
@@ -9,13 +10,13 @@ const Projects = (props) => {
         <a
           className="project-link"
           href={props.projectLink}
+          onClick={(e) => EventsTracker(props.projectName, props.projectLink)}
           target="_blank"
           rel="noopener noreferrer"
         >
           <i className="fa-solid fa-link"></i> View Project
         </a>
       </div>
-      
     </>
   );
 };
