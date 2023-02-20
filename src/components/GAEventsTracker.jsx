fb093c5ad573803e239be9fa38a957454522240a
Tony Maluki <tonymaluki@gmail.com>
2023-02-20 15:24:06 +0300

    wrote a function to relay events to google analytics

---
+++ src/components/GAEventsTracker.jsx
@@ -0,0 +1,14 @@
+import ReactGA from "react-ga";
+
+const GAEventsTracker = (category = "Event category") => {
+  const trackEvent = (action = "action", label = "label") => {
+    ReactGA.event({
+      category,
+      action,
+      label,
+    });
+  };
+  return trackEvent;
+};
+
+export default GAEventsTracker;
