e93c0606c198907091a5c2835b8531a98c95b081
Tony Maluki <tonymaluki@gmail.com>
2023-03-08 17:56:14 +0300

    added wakatime badge

--- src/components/Home/Home3.jsx
+++ src/components/Home/Home3.jsx
@@ -1,5 +1,7 @@
 import Socials from "../Socials";
+import GAEventsTracker from "../GAEventsTracker";
 const Home3 = () => {
+  const EventsTracker = GAEventsTracker("External Links");
   return (
     <section className="contacts">
       <div className="text-wrapper">
@@ -9,6 +11,16 @@ const Home3 = () => {
         </h4>
       </div>
       <Socials />
+      <a
+        href="https://wakatime.com/@3b96c015-dfc0-446d-8b3a-bc2fbefa9b31"
+        target="_blank"
+        onClick={(e) => EventsTracker("WakaTime", "WakaTime Stats")}
+      >
+        <img
+          src="https://wakatime.com/badge/user/3b96c015-dfc0-446d-8b3a-bc2fbefa9b31.svg"
+          alt="Total time coded since Jan 31 2022"
+        />
+      </a>
     </section>
   );
 };
