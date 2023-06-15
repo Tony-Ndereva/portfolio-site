7495d2c1e1afae9c94e92e8fdac1d1c3d9c6b83b
Tony Maluki <tonymaluki@gmail.com>
2023-06-15 13:51:30 +0300

    uniformity in project names

--- src/resumeData.js
+++ src/resumeData.js
@@ -10,7 +10,7 @@ export default [
   {
     projects: {
       id: nanoid(),
-      projectName: "ICT Authority KENYA",
+      projectName: "ict authority kenya",
       projectImage: icta,
       projectDescription:
         "The Information and Communication Technology (ICT) Authority is a State Corporation under the Ministry of Information Communication and Technology in Kenya. I was part of a 2-men team which designed, developed and implemented the new official website from client to the server side features of the system.",
@@ -21,7 +21,7 @@ export default [
   {
     projects: {
       id: nanoid(),
-      projectName: "Connected KENYA",
+      projectName: "connected kenya",
       projectImage: connected,
       projectDescription:
         " The Connected Kenya Summit is a an ICT thought leadership gathering that brings together over 1,000 regional and global ICT industry thought leaders, key policy makers, high profile Industry champions and executives, financiers and ICT innovators, for a series of high-level discussions geared towards nurturing innovative ideas, investments and partnerships that spur the realization of Kenya’s development agenda leveraging on technology. I was part of a 2-man team which i was specifically mandated to design and implement the application's database.",
@@ -32,7 +32,7 @@ export default [
   {
     projects: {
       id: nanoid(),
-      projectName: "KASNEB WEB STUDENTS & STAFF PORTAL",
+      projectName: "kasneb web students & staff portal",
       projectImage: kasneb,
       projectDescription:
         "This is an online web portal that let students the school's resources such as grades, and school fees invoices. I was mandated in the quality assurance and analysis for this system.",
@@ -42,7 +42,7 @@ export default [
   {
     projects: {
       id: nanoid(),
-      projectName: "YBK Ltd",
+      projectName: "ybk Ltd",
       projectImage: ybk,
       projectDescription:
         "YBK is a premium Gift Shop that deals with designing personalized Gifts ,Branding and printing co-operatw merchandise. I developed the main catalogue site that showcases different services offered and their prices. ",
@@ -52,7 +52,7 @@ export default [
   {
     projects: {
       id: nanoid(),
-      projectName: "Barbershop booking system",
+      projectName: "barbershop booking system",
       projectImage: barbershop,
       projectDescription:
         "This is an online booking system whereby enables clients see, book and pay for haircuts, spa work, and other beauty services.",
