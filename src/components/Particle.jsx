b5593c28462a8391278400abef856a958685f86e
Tony Maluki <tonymaluki@gmail.com>
2023-07-04 12:44:33 +0300

    cleaning up unused code

--- src/components/Particle.jsx
+++ src/components/Particle.jsx
@@ -1,4 +1,3 @@
-import { random } from "nanoid";
 import { useCallback } from "react";
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
@@ -8,36 +7,6 @@ const Particle = () => {
     await loadFull(engine);
   });
 
-  // return (
-  //   <Particles
-  //     options={{
-  //       particles: {
-  //         color: {
-  //           value: "#fff",
-  //         },
-  //         number: {
-  //           value: 30,
-  //         },
-  //         opacity: {
-  //           value: { min: 0.3, max: 0.4 },
-  //         },
-  //         shape: {
-  //           type: "circle",
-  //         },
-  //         size: {
-  //           value: { min: 1, max: 5 },
-  //         },
-  //         move: {
-  //           direction:random,
-  //           enable: true,
-  //           speed: { min: 0.01, max: 0.5 },
-  //           straight: false,
-  //         },
-  //       },
-  //     }}
-  //     init={init}
-  //   />
-  // );
 
   return (
     <Particles
