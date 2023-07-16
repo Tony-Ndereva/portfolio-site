93be546bece8a190f5267be6961329f00d3a8bf1
Tony <tonymaluki@gmail.com>
2023-07-16 13:09:33 +0300

    refactored code

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
+import React, { useEffect, useState, useContext } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import ReactGA4 from "react-ga4";
+
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
-import { Routes, Route, useLocation } from "react-router-dom";
-import resumeData from "./resumeData";
 import Particle from "./components/Particle";
 import Tonydetails from "./Tonydetails";
-import { useContext, useEffect, useState } from "react";
+import resumeData from "./resumeData";
 import { ThemeContext } from "./components/ThemeChanger";
-import ReactGA4 from "react-ga4";
-
-
-
 
 const TRACKING_ID = import.meta.env.VITE_REACT_APP_GA4_TRACKING_ID;
 
@@ -30,24 +28,20 @@ const App = () => {
 
   const [projectsData, setProjectsData] = useState([]);
   const [userData, setUserData] = useState([]);
-  const projects = projectsData.map((each) => {
-    return <Projects {...each.projects} key={each.projects.id} />;
+  const projects = projectsData.map((project, index) => {
+    return <Projects {...project.projects} key={index + 1} />;
   });
   useEffect(() => {
     setProjectsData(resumeData);
     setUserData(Tonydetails);
   }, []);
 
-  const footer = userData.map((each) => {
-    return <Footer {...each.details} key={each.details.id} />;
+  const footer = userData.map((user, index) => {
+    return <Footer {...user.details} key={index + 1} />;
   });
-  const home = userData.map((each) => {
-    return <Home {...each.details} key={each.details.id} />;
+  const home = userData.map((user, index) => {
+    return <Home {...user.details} key={index + 1} />;
   });
-
-  // The code just below gets the props for lightMode
-  //const {lightMode} = useContext(ThemeContext)
-  //console.log(lightMode)
 
   const { theme, setTheme } = useContext(ThemeContext);
 
