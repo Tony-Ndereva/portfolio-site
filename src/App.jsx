52777e3cc5587b50e048c0520979992e0a17c96d
Tony Maluki <tonymaluki@gmail.com>
2023-07-20 21:25:10 +0300

    firebase config

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
 import React, { useEffect, useState, useContext } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
-import ReactGA4 from "react-ga4";
+import ReactGA4 from "react-ga4"; // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: import.meta.env.API_KEY,
+  authDomain: import.meta.env.AUTH_DOMAIN,
+  projectId: import.meta.env.PROJECT_ID,
+  storageBucket: import.meta.env.STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.MESSAGING_SENDER_ID,
+  appId: import.meta.env.APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar";
